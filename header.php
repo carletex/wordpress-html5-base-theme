ef38f8241dec9440e4e25e79b357c0f5801d85d2
carletex <oceanrdn@gmail.com>
2014-03-17 21:03:27 +0100

    echo template uri, normalize.css was not loading

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <title><?php bloginfo('name'); ?> | <?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
   <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-  <link rel="stylesheet" href="<?php get_template_directory_uri() . '/css/normalize.css' ?>" type="text/css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/normalize.css' ?>" type="text/css" />
   <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <?php wp_head(); ?>
